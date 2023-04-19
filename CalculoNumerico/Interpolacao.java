98b62574647316eda2db1f006a0303d2328176ed
G4ldin0 <joao.duarte31219@alunos.ufersa.edu.br>
2023-04-18 21:36:47 -0300

    implementação do método de Lagrange

--- CalculoNumerico/Interpolacao.java
+++ CalculoNumerico/Interpolacao.java
@@ -1,5 +1,46 @@
 package CalculoNumerico;
 
 public class Interpolacao{
+
+   private static Matriz Origem;
+   private static Matriz Retorno;
    
+
+   public static Matriz Lagrange(Matriz origem)
+   {
+      Origem = origem;
+      Retorno = new Matriz(1, origem.Linha());
+
+      //{ x = {-1,0,2}, y = {4,1,-1}}
+
+      //Pn(x) = somatorio(n/k=0)(yk * Lk(x))
+      //Lk(x) = (Produtorio(n/j=0,j!=k)(x-xj))/(Produtorio(n/j=0,j!=k)(xk-xj))
+
+
+      Origem.ExibirMatriz();
+      Retorno.ExibirMatriz();
+
+      for(int i = 0; i < origem.Linha(); i++) L(i);
+      
+      for(int i = 0; i < origem.Linha(); i++) Retorno.set(Retorno.get(i,0) * Origem.get(1, i), i, 0);
+
+      return Retorno;
+   }
+
+   private static void L(int ordem)
+   {
+      double controle = 
+         Origem.get(0, ordem) * Origem.get(0, ordem)
+       - Origem.get(0, ordem) * Origem.get(0,  (ordem + 2)% Origem.Linha()) 
+       - Origem.get(0, (ordem + 1)% Origem.Linha()) * Origem.get(0, ordem) 
+       + Origem.get(0, (ordem + 1)% Origem.Linha()) * Origem.get(0, (ordem + 2) % Origem.Linha());
+
+
+      Retorno.set(Retorno.get(0,0) + 1 / controle,0, 0);
+      Retorno.set(Retorno.get(1,0) + (- Origem.get(0, (ordem + 1)% Origem.Linha()) - Origem.get(0,(ordem + 2)% Origem.Linha()) / controle),1, 0);
+      Retorno.set(Retorno.get(2,0) + (Origem.get(0, (ordem + 1)% Origem.Linha()) * Origem.get(0, (ordem + 2)% Origem.Linha()) / controle),2, 0);
+
+
+   }
+
 }
--- Main.java
+++ Main.java
@@ -3,10 +3,9 @@ import CalculoNumerico.*;
 public class Main {
    public static void main(String[] args) {
       
-      Matriz exemplo = new Matriz(new double[][]{{10,2,1,7}, {1,5,1,-8}, {2,3,10,6} });
-      exemplo.ExibirMatriz();
-      exemplo = OperacoesMatriciais.GaussJacobi(exemplo);
-      exemplo.ExibirMatriz();
+      Matriz exemplo = new Matriz(new double[][]{{-1,0,2},{4,1,-1}});
+      
+      Interpolacao.Lagrange(exemplo).ExibirMatriz();
    }
    
 }
