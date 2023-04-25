cc0fa57b1572922c01d7a3d666875292b086eb39
G4ldin0 <joao.duarte31219@alunos.ufersa.edu.br>
2023-04-25 16:21:08 -0300

    add: metodo determinante + inicio inversao matricial

--- CalculoNumerico/Matriz.java
+++ CalculoNumerico/Matriz.java
@@ -78,7 +78,7 @@ public class Matriz{
    public int Coluna() { return coluna; }
 
    public void ExibirMatriz(){
-      System.out.println("Matriz: ");
+      System.out.println("Matriz: " + " " + linha + "x" + coluna);
       System.out.println("[");
       for(int i = 0; i < coluna; i++){
          for(int j = 0; j < linha; j++){
@@ -87,6 +87,107 @@ public class Matriz{
          System.out.println();
       }
       System.out.println("]");
+   }
+
+   public void escalar(double valor){
+      for(int i = 0; i < coluna; i++){
+         double[] placeholder = new double[linha];
+         for(int j = 0; j < linha; j++){
+            placeholder[j] = valor * matriz.get(i)[j];
+         }
+         matriz.set(i, placeholder);
+      }
+   }
+
+   public boolean matricial(Matriz valor){
+
+      if(valor.coluna != linha)
+         return false;
+      else{
+         ArrayList<double[]> placeholder = new ArrayList<double[]>();
+
+         for(int i = 0; i < coluna; i++)
+         {
+            double[] vetor = new double[valor.linha];
+            for(int j = 0; j < valor.linha; j++)
+            {
+               double soma = 0;
+               for(int k = 0; k < linha; k++)
+               {
+                  soma += matriz.get(i)[k] * valor.get(k)[j];
+               }
+               vetor[j] = soma;
+            }
+            placeholder.add(vetor);
+         }
+
+         matriz = placeholder;
+         linha = valor.linha;
+
+         return true;
+      }
+
+   }
+
+   public void transpor(){
+      //nova ED placeholder
+      ArrayList<double[]> placeholder = new ArrayList<double[]>();
+
+      //pega o elemento da posição contrária e guarda na nova posição do placeholder
+      for(int i = 0; i < linha; i++){
+         double[] novo = new double[linha];
+         for(int j = 0; j < coluna; j++){
+            novo[j] = matriz.get(j)[i];
+         }
+         placeholder.add(novo);
+      }
+      
+      //aplica os novos valores nos atributos
+      matriz = placeholder;
+      int ph = coluna;
+      coluna = linha;
+      linha = ph;
+   }
+
+   public double determinante(){
+      
+      Matriz superior = OperacoesMatriciais.DecomposicaoLU(this)[1];
+      
+      double valor = superior.get(0,0);
+
+      for(int i = 1; i < linha; i++)
+         valor *= superior.get(i,i);
+
+      return valor;
+   }
+
+   public void inverter(){
+      //matriz que, ao multiplicada pela matriz origem, gerará a matriz identidade
+      ArrayList<double[]> placeholder = new ArrayList<double[]>();
+
+
+      /*
+       *  -1  4
+       *   4  1
+       * 
+       *   x  y  
+       *   w  z  
+       * 
+       *   -1*x + 4*w = 1  -1*y + 4*z = 0
+       *    4*x + 1*w = 0   4*y + 1*z = 1
+       * 
+       *   x = (1 - 4w)/-1  y = (0 - 4z)/-1
+       *   w = (0 - 4x)/1   z = (1 - 4y)/1
+       * 
+       */
+
+            
+      //aplica os novos valores nos atributos
+      matriz = placeholder;
+      int ph = coluna;
+      coluna = linha;
+      linha = ph;
+
    }
 
    public double NormaInfinita(){
--- Main.java
+++ Main.java
@@ -3,9 +3,12 @@ import CalculoNumerico.*;
 public class Main {
    public static void main(String[] args) {
       
-      Matriz exemplo = new Matriz(new double[][]{{-1,0,2},{4,1,-1}});
-      
-      Interpolacao.Lagrange(exemplo).ExibirMatriz();
+      Matriz exemplo = new Matriz(new double[][]{{2,1},{5,3}});
+
+      exemplo.ExibirMatriz();
+      System.out.println(exemplo.determinante());
+
+
    }
    
 }
